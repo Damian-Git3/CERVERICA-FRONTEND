0c9d443fe35f5bd22cc9f1078f5e826f11ecbd24
Mario-Vargas-ECO <21000036@alumnos.utleon.edu.mx>
2024-10-02 01:37:54 -0600

    Prueba funcional de Mensaje Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
 import { PrimeModule } from './components/prime/prime.module';
 import { RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { Messaging } from '@angular/fire/messaging';
+import { FcmService } from './services/firebase/fcm.service';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +14,17 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   imports: [RouterOutlet, PrimeModule, FontAwesomeModule],
 })
 export class AppComponent {
-  title = 'CervericaLanding';
+  title = 'cerverica-landing';
+  private messaging = inject(Messaging);
 
-  constructor(private primengConfig: PrimeNGConfig) {}
+  constructor(private primengConfig: PrimeNGConfig, private fcmService: FcmService) {}
 
   ngOnInit() {
     this.primengConfig.ripple = true;
+    this.fcmService.requestPermission();
+    this.fcmService.messages.subscribe((message) => {
+      console.log('New message received:', message);
+      // Aquí puedes manejar la notificación en la UI
+    });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err),
 );
+
+// Register the service worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./firebase-messaging-sw.js')
+    .then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }).catch((err) => {
+      console.log('Service Worker registration failed:', err);
+    });
+}
